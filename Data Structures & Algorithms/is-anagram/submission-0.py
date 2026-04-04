0442c8593af78cfbc7f552d56812a94f37006138
akshayshama <akshayhonnappa04@gmail.com>
2026-04-04 20:11:49 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s)!=len(t):
+            return False
+
+        countS,countT={},{}
+        # creating and adding num to hashmaps
+        for i in range(len(t)):
+            countS[s[i]]=1+countS.get(s[i],0)
+            countT[t[i]]=1+countT.get(t[i],0)
+
+        # to check the count of elements 
+        for c in countS:
+            if countS[c]!=countT.get(c,0):
+                return False
+        return True
+
+        
+        
